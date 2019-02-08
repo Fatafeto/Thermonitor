ca7b948cfb5381336fd34c5da8dd98c263b1f750
Fatafeto <fatmahelmy98@gmail.com>
2019-02-08 05:36:39 +0200

    Thermonitor Milestone 1

--- app/src/main/java/com/example/thermonitor/ListActivity.java
+++ app/src/main/java/com/example/thermonitor/ListActivity.java
@@ -4,15 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.widget.TextView;
 
 public class ListActivity extends AppCompatActivity {
 
     ListView listView;
+    String[] listElements = {"Android" , "iPhone" , "BlackBerry" , "Windows" , "Linux"};
+    int[] images = {R.drawable.android , R.drawable.apple , R.drawable.blackberry, R.drawable.windows , R.drawable.linux};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +23,11 @@ public class ListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
 
         listView = (ListView)findViewById(R.id.listView);
-        ArrayList<String> listElements = new ArrayList<>();
-        listElements.add("Android");
-        listElements.add("iPhone");
-        listElements.add("BlackBerry");
-        listElements.add("Windows");
-        listElements.add("Linux");
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,listElements);
-        listView.setAdapter(arrayAdapter);
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -36,6 +35,35 @@ public class ListActivity extends AppCompatActivity {
                 startActivity(myIntent);
 
             }
+
         });
+
+    }
+
+    class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view  = getLayoutInflater().inflate(R.layout.image_list , null);
+            ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+            TextView textView = (TextView) view.findViewById(R.id.textView);
+            imageView.setImageResource(images[position]);
+            textView.setText(listElements[position]);
+            return view;
+        }
     }
 }
--- app/src/main/java/com/example/thermonitor/RegisterActivity.java
+++ app/src/main/java/com/example/thermonitor/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -14,40 +15,36 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApiNotAvailableException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuthException;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    Button button1;
-    Button button2;
+    Button button;
     EditText inputMail;
     EditText inputPassword;
     FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
+        setContentView(R.layout.activity_register);
+        button = (Button) findViewById(R.id.button);
         inputMail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
         addListenerOnButton();
         firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
     }
 
     public void addListenerOnButton() {
         final Context context = this;
-
-        button1.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = inputMail.getText().toString();
-                final String password = inputPassword.getText().toString();
+                String email = inputMail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
 
                 if(TextUtils.isEmpty(email)) {
                     Toast.makeText(getApplicationContext() , "Please enter an e-mail" , Toast.LENGTH_SHORT).show();
@@ -57,44 +54,34 @@ public class LoginActivity extends AppCompatActivity {
                 if(TextUtils.isEmpty(password)) {
                     Toast.makeText(getApplicationContext() , "Please enter a password" , Toast.LENGTH_SHORT).show();
                     return;
+
                 }
 
                 if(password.length() < 8) {
-                    Toast.makeText(getApplicationContext() , "Password is at least 8 characters" , Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext() , "Password length is at least 8 characters" , Toast.LENGTH_SHORT).show();
                     return;
                 }
 
+                Toast.makeText(RegisterActivity.this , "Creating account ....." , Toast.LENGTH_SHORT).show();
 
-                    Toast.makeText(LoginActivity.this , "Signing in ..." , Toast.LENGTH_SHORT).show();
-
-                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(!(task.isSuccessful()))
-                            Toast.makeText(LoginActivity.this , task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-
+                            Toast.makeText(RegisterActivity.this , task.getException().getMessage() , Toast.LENGTH_SHORT).show();
                         if(task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this , "Successful" , Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(context, ListActivity.class);
                             startActivity(intent);
-                            finish();
                         }
 
 
                     }
                 });
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, RegisterActivity.class);
-                startActivity(intent);
 
 
             }
         })
-
 
 
         ;}
